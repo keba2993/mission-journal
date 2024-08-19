12a944309ed819e88d4e5f89ae4bfe9ae0c01ef9
Kevin Barone <72464586+keba2993@users.noreply.github.com>
2024-08-19 15:20:53 -0600

    adding proper descriptions

--- Mission Trip Journal/src/data/data.ts
+++ Mission Trip Journal/src/data/data.ts
@@ -6,7 +6,7 @@ export default [
 		startDate: new Date(2022, 11, 25),
 		endDate: new Date(2022, 11, 31),
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			'For my first ever mission trip, I joined a group of friends to work alongside Amor ministries, joinging them in their mission to serve and build houses for the poverty-stricken communities in Tijuana. I aided in the construction efforts of four different homes, building one home completely from scratch and repairing/finishing three others. It was honor to serve the Lord through manual labor and provide these families with a safe place to live.',
 		imageUrl: '/amor.png',
 		ministryUrl: 'https://amor.org/',
 		ministryName: 'Amor Ministries',
@@ -18,7 +18,7 @@ export default [
 		startDate: new Date(2023, 11, 25),
 		endDate: new Date(2024, 0, 1),
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			'In the winter of 2023, I traveled to Cuba with my girlfriend and her dad to aid the ministry efforts there. This mission trip consisted of meeting with, encouraging, and learning from local house church pastors already at work in Cuba, providing those living in poverty with food and other gifts, and attending local church services. I learned so much about being fully dependant on the Lord and trusting him to provide everything that you would need.',
 		imageUrl: '/bold-hope.jfif',
 		ministryUrl: 'https://boldhope.org/',
 		ministryName: 'Bold Hope',
@@ -30,7 +30,7 @@ export default [
 		startDate: new Date(2024, 5, 16),
 		endDate: new Date(2024, 5, 21),
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			'I had the honor of volunteering at the Royal Family Kids Camp for foster children as the Breakfast Club host and a camp Guide. Throughout the week I participated in various summer camp activities with the kids, such as archery, fishing, and carpentry all while sharing the love of Christ with them. In Breakfast Club, I had the opportunity to lead the kids through various object lessons surrounding the camps theme of being a winderfully made child of God.',
 		imageUrl: '/rfkc.png',
 		ministryUrl: 'https://arvadaroyalfamilykids.org/',
 		ministryName: 'Royal Family Kids Arvada',
